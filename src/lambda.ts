0b93c633e44fa253ea768dd570bfb411f0741ba3
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-24 11:53:41 +1200

    Revert "Add mimeTypes"
    This reverts commit 1d343bce250baf5ed42ffbdec0a076bbbf1eeb48.

--- src/lambda.ts
+++ src/lambda.ts
@@ -7,23 +7,13 @@ import cors from "@middy/http-cors";
 import httpErrorHandler from "@middy/http-error-handler";
 
 const binaryMimeTypes = [
-  "application/javascript",
-  "application/json",
   "application/octet-stream",
-  "application/xml",
   "font/eot",
   "font/opentype",
   "font/otf",
   "image/jpeg",
   "image/png",
   "image/svg+xml",
-  "text/comma-separated-values",
-  "text/css",
-  "text/html",
-  "text/javascript",
-  "text/plain",
-  "text/text",
-  "text/xml",
 ];
 
 const server = createServer(app, null, binaryMimeTypes);
