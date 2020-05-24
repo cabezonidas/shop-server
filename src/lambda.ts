e23513b0f4507d83b3d4708dfb4cb7733aafff5a
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-24 12:10:02 +1200

    Fix binary media types

--- src/lambda.ts
+++ src/lambda.ts
@@ -6,17 +6,7 @@ import middy from "@middy/core";
 import cors from "@middy/http-cors";
 import httpErrorHandler from "@middy/http-error-handler";
 
-const binaryMimeTypes = [
-  "application/octet-stream",
-  "font/eot",
-  "font/opentype",
-  "font/otf",
-  "image/jpeg",
-  "image/png",
-  "image/svg+xml",
-];
-
-const server = createServer(app, null, binaryMimeTypes);
+const server = createServer(app, null);
 
 export const handler = middy(async (event: any, context: Context) => {
   context.callbackWaitsForEmptyEventLoop = false;
