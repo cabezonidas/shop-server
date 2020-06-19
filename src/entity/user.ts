9d7023d889dcb6ee75432cf225647db75b21d637
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-06-20 10:56:57 +1200

    Extend user object

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -1,9 +1,20 @@
 import { Entity, Column, BaseEntity, ObjectIdColumn, ObjectID } from "typeorm";
-import { ObjectType, Field } from "type-graphql";
+import { ObjectType, Field, Int } from "type-graphql";
+
+@ObjectType()
+class Description extends BaseEntity {
+  @Field(() => String)
+  @Column()
+  public localeId: string;
+
+  @Field(() => String)
+  @Column()
+  public text: string;
+}
 
 @ObjectType()
 @Entity("users")
-export class User extends BaseEntity {
+export class User extends Description {
   @Field(() => String)
   @ObjectIdColumn()
   public _id: ObjectID;
@@ -11,6 +22,50 @@ export class User extends BaseEntity {
   @Field()
   @Column()
   public email: string;
+
+  @Field(() => [String], { nullable: true })
+  @Column()
+  public roles: [string];
+
+  @Field(() => Int, { nullable: true })
+  @Column()
+  public dob: number;
+
+  @Field(() => String, { nullable: true })
+  @Column()
+  public name: string;
+
+  @Field(() => String, { nullable: true })
+  @Column()
+  public imageUrl: string;
+
+  @Field(() => String, { nullable: true })
+  @Column()
+  public linkedin: string;
+
+  @Field(() => String, { nullable: true })
+  @Column()
+  public whatsapp: string;
+
+  @Field(() => String, { nullable: true })
+  @Column()
+  public instagram: string;
+
+  @Field(() => String, { nullable: true })
+  @Column()
+  public facebook: string;
+
+  @Field(() => String, { nullable: true })
+  @Column()
+  public messenger: string;
+
+  @Field(() => String, { nullable: true })
+  @Column()
+  public github: string;
+
+  @Field(() => [Description], { nullable: true })
+  @Column()
+  public description: [Description];
 
   @Column()
   public password: string;
